ec48b9b331c04e2fc1fa8895a493a760bad43c00
wafforyu <wafforyu@gmail.com>
2022-01-08 19:14:59 +0800

    add enter button checking

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const rng = () => Math.trunc(Math.random() * 1000);
 let randomNumber = rng();
 let score = 20;
 let highscore = 0;
+let gameOver = false;
 
 const changeMessage = (message) => {
   document.querySelector(".message").textContent = message;
@@ -13,8 +14,7 @@ const changeScore = (score) => {
   document.querySelector(".score").textContent = score;
 };
 
-// on check button click
-document.querySelector(".check").addEventListener("click", () => {
+const processGame = () => {
   const guess = document.querySelector(".guess").value;
 
   if (!guess) {
@@ -23,7 +23,8 @@ document.querySelector(".check").addEventListener("click", () => {
     changeMessage("🎊 Correct Number!");
     document.querySelector(".number").textContent = randomNumber;
     document.querySelector("body").style.backgroundColor = "#60b437";
-
+    document.querySelector(".check").classList.add("disabled");
+    gameOver = true;
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = score;
@@ -33,16 +34,26 @@ document.querySelector(".check").addEventListener("click", () => {
     changeScore(--score);
     if (score < 1) {
       changeMessage("YOU LOST THE GAME!");
+      document.querySelector(".check").classList.add("disabled");
+      gameOver = true;
     }
   }
+};
+
+// on check button click
+document.querySelector(".check").addEventListener("click", processGame);
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && !gameOver) processGame();
 });
 
+// on reset button click
 document.querySelector(".again").addEventListener("click", () => {
   document.querySelector("body").style.backgroundColor = "var(--bs-gray-900)";
   document.querySelector(".number").textContent = "?";
+  changeMessage("START GUESSING!");
   score = 20;
-  document.querySelector(".score").textContent = score;
+  changeScore(score);
   randomNumber = rng();
+  document.querySelector(".check").classList.remove("disabled");
+  gameOver = false;
 });
-
-// to add bind enter to check guess,
